c3b262aba9bb53328c9ba5e69663ab8030cc36fe
dollar <dollar0712@wwoingdasd.xyz>
2024-05-02 17:41:25 +0400

    修改新版本web3

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import vitePluginInspect from "vite-plugin-inspect"
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
-
+  plugins: [react(),vitePluginInspect()],
+  // 设置路径别名
+  resolve: {
+    alias: {
+      "@": "/src", // 正确配置的路径别名，将 @ 映射到 /src
+    },
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
